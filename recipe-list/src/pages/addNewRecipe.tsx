c1bf7b0f588259a8f7395dd9ad03208398044536
Mareks M <45588297+MrMarex@users.noreply.github.com>
2023-02-26 20:05:25 +0200

    Small improvements
    Small improvements and added json file to import data to database.

--- recipe-list/src/pages/addNewRecipe.tsx
+++ recipe-list/src/pages/addNewRecipe.tsx
@@ -1,87 +1,124 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import axios from 'axios';
 import MainLayout from '@/layouts/MainLayout/MainLayout';
 import endpoint from '@/utils/endpoint';
 import router from 'next/router';
 import style from '../styles/AddNewRecipePage.module.scss';
 
-export default function AddNewRecipeForm () {
-    const [title, setTitle] = useState('');
-    const [image, setImage] = useState('');
-    const [ingredients, setIngredients] = useState('');
-    const [recipe, setRecipe] = useState('');
-    const [category, setCategory] = useState('');
+interface FormData {
+  title: string;
+  image: string;
+  ingredients: string;
+  recipe: string;
+  category: string;
+}
 
-    const handleFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const newRecipe = {
-            title,
-            image,
-            ingredients,
-            recipe,
-            category,
-        };
-        try {
-            const response = await axios.post(`${endpoint}/addRecipe`, newRecipe);
-            console.log('response', response);
-            router.push('/recipes');
-        } catch (error) {
-            console.error('error', error);
-        }
+interface ResponseData {
+  message: string;
+}
+
+export default function AddNewRecipeForm () {
+    const [formData, setFormData] = useState<FormData>({
+        title: '',
+        image: '',
+        ingredients: '',
+        recipe: '',
+        category: '',
+    });
+
+    const {
+        title, image, ingredients, recipe, category,
+    } = formData;
+
+    const handleFormSubmit = useCallback(
+        async (event: React.FormEvent<HTMLFormElement>) => {
+            event.preventDefault();
+            try {
+                const response = await axios.post<ResponseData>(`${endpoint}/addRecipe`, formData);
+                console.log('response', response);
+                router.push('/recipes');
+            } catch (error) {
+                console.error('error', error);
+            }
+        },
+        [formData],
+    );
+
+    // eslint-disable-next-line max-len
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = event.target;
+        setFormData((prevState) => ({ ...prevState, [name]: value }));
     };
 
     return (
-        <MainLayout title='Add new recipe'>
+        <MainLayout title="Add new recipe">
             <form onSubmit={handleFormSubmit} className={style.newRecipeForm}>
-                <label htmlFor="title" className={style.formLabel}>Title: <br />
+                <label htmlFor="title" className={style.formLabel}>
+          Title: <br />
                     <input
                         type="text"
                         id="title"
                         placeholder="Pancakes..."
+                        name="title"
                         value={title}
+                        required
                         className={style.formInput}
-                        onChange={(e) => setTitle(e.target.value)}
+                        onChange={handleInputChange}
                     />
                 </label>
-                <label htmlFor="image" className={style.formLabel}>Image URL: <br />
+                <label htmlFor="image" className={style.formLabel}>
+          Image URL: <br />
                     <input
                         type="text"
                         id="image"
                         placeholder="https://image.url..."
+                        name="image"
                         value={image}
+                        required
                         className={style.formInput}
-                        onChange={(e) => setImage(e.target.value)}
+                        onChange={handleInputChange}
                     />
                 </label>
-                <label htmlFor="ingredients" className={style.formLabel}>Ingredients: <br />
+                <label htmlFor="ingredients" className={style.formLabel}>
+          Ingredients: <br />
                     <textarea
                         id="ingredients"
                         placeholder="Sour cream, sugar, eggs, milk..."
+                        name="ingredients"
                         value={ingredients}
+                        required
                         className={style.formTextArea}
-                        onChange={(e) => setIngredients(e.target.value)}
+                        onChange={handleInputChange}
                     />
                 </label>
-                <label htmlFor="recipe" className={style.formLabel}>Recipe: <br />
+                <label htmlFor="recipe" className={style.formLabel}>
+          Recipe: <br />
                     <textarea
                         id="recipe"
                         placeholder="Mix 2 eggs with milk..."
+                        name="recipe"
                         value={recipe}
+                        required
                         className={style.formTextArea}
-                        onChange={(e) => setRecipe(e.target.value)}
+                        onChange={handleInputChange}
                     />
                 </label>
-                <label htmlFor="category" className={style.formLabel}>Category: <br />
+                <label htmlFor="category" className={style.formLabel}>
+          Category: <br />
                     <input
                         type="text"
                         id="category"
                         placeholder="Pancakes..."
+                        name="category"
                         value={category}
+                        required
                         className={style.formInput}
-                        onChange={(e) => setCategory(e.target.value)}
+                        onChange={handleInputChange}
                     />
                 </label>
-                <button type="submit" className={style.button}>Add new recipe</button>
+                <button type="submit" className={style.button}>
+          Add new recipe
+                </button>
             </form>
         </MainLayout>
     );
--- recipe-list/src/pages/index.tsx
+++ recipe-list/src/pages/index.tsx
@@ -8,7 +8,14 @@ export default function Home () {
                 Home page.
             </h1>
             <p className={style.paragraph}>
-                This is page for different recipes.
+                Welcome to the Recipe Blog, where you can explore a variety of delicious
+                recipes to satisfy your taste buds! Our blog features recipes for all occasions
+                and meals, including breakfast, lunch, dinner, and dessert. From classic comfort
+                foods to unique and exotic dishes, our recipes are sure to impress and inspire you
+                to get creative in the kitchen. Each recipe is carefully crafted and tested,
+                ensuring that they are easy to follow and produce great results. So whether
+                you`re a seasoned cook or just starting out, we have something for everyone.
+                Browse our collection of recipes today and get ready to whip up something delicious!
             </p>
         </MainLayout>
     );
