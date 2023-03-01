5664c0e30d6d90eeed99ae9acea1be805c5aafee
Mareks M <45588297+MrMarex@users.noreply.github.com>
2023-03-01 19:06:49 +0200

    Added 23rd homework
    Angular check list of 4 items that can be checked or unchecked and that changes total price.

---
+++ angular-check-list/src/app/service-list/service-list.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+interface ServiceItem {
+  name: string;
+  price: number;
+  checked: boolean;
+}
+
+@Component({
+  selector: 'app-service-list',
+  templateUrl: './service-list.component.html',
+  styleUrls: ['./service-list.component.css']
+})
+export class ServiceListComponent {
+  services: ServiceItem[] = [
+    { name: 'Web Development', price: 300, checked: false },
+    { name: 'Design', price: 400, checked: false },
+    { name: 'Integration', price: 250.00, checked: false },
+    { name: 'Training', price: 220.00, checked: false }
+  ];
+
+  total: number = 0;
+
+  updateTotal() {
+    this.total = this.services.filter(service => service.checked)
+                           .reduce((sum, service) => sum + service.price, 0);
+  }
+
+  onServiceClick(service: ServiceItem) {
+    service.checked = !service.checked;
+    this.updateTotal();
+  }
+}
